76e6365333a38c55c8616a3197e09008455cb413
EralChen <eralchen@foxmail.com>
2025-03-12 11:07:15 +0800

    pref: clear

--- app/src/api/init.ts
+++
@@ -1,6 +0,0 @@
-import { restFetch as platformRestFetch } from '@skzz/platform/shared/fetch/platform'
-import { restFetch as publicRestFetch } from '@skzz/platform/shared/fetch/public'
-
-publicRestFetch.baseURL = `${import.meta.env.VITE_BASE_URL}/data`
-platformRestFetch.baseURL = import.meta.env.VITE_DEFAULT_PLATFORM_API
-platformRestFetch.DEV = import.meta.env.DEV
--- app/src/views/home/test.ts
+++
@@ -1,8 +0,0 @@
-import { defineComponent } from 'vue'
-
-export const ArrayFragment = defineComponent({
-  name: 'ArrayFragment',
-  setup (props, { slots }) {
-    return () => slots.default?.()
-  },
-})
