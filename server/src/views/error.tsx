214812ed1681969fe1329680a2e40f1c14e8854b
EralChen <eralchen@foxmail.com>
2025-04-27 17:36:04 +0800

    pref: error demo

--- server/src/views/error.tsx
+++ server/src/views/error.tsx
@@ -1,9 +1,19 @@
+import { VkResponse } from '@vunk-server/components/response'
 import { defineComponent } from '@vunk-server/jsx-runtime'
 
 export default defineComponent({
   name: 'TestError',
   setup () {
-    throw new Error('This is an error!')
-    return () => null
+    // 随机抛错
+    const random = Math.random()
+    if (random > 0.5) {
+      throw new Error('This is an error!')
+    }
+
+    return () => (
+      <VkResponse>
+        <name>cx</name>
+      </VkResponse>
+    )
   },
 })
